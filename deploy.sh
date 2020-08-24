b27490d48fe71d2b60631f6657003c8a0d8d377b
Samuli <samuli841@gmail.com>
2020-08-24 22:37:40 +0300

    ssl added

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,6 @@ sudo mkdir /home/seronen/.ssh/
 sudo cat ~/roger/assets/ssh/authorized_keys > /home/seronen/.ssh/
 
 sudo service ssh restart
-sudo service sshd restart
 sudo service networking restart
 sudo ifup eth0
 
@@ -31,8 +30,14 @@ sudo ufw allow 50683/tcp
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw reload
-sudo service sshd restart
 sudo service ssh restart
+
+#Setup Portsentry for protection against port scanning
+rm -rf /etc/default/portsentry
+cp ~/roger/assets/portsentry/portsentry /etc/default/
+rm -rf /etc/portsentry/portsentry.conf
+cp ~/roger/assets/portsentry/portsentry.conf /etc/portsentry/
+sudo service portsentry restart
 
 #Disable services not needed
 sudo systemctl disable console-setup.service
@@ -41,3 +46,32 @@ sudo systemctl disable apt-daily.timer
 sudo systemctl disable apt-daily-upgrade.timer
 sudo systemctl disable syslog.service
 
+#Setup scripts
+cp -r ~/roger/assets/scripts/ ~/
+{ crontab -l -u root; echo '0 4 * * SUN sudo ~/scripts/update.sh'; } | crontab -u root -
+{ crontab -l -u root; echo '@reboot sudo ~/scripts/update.sh'; } | crontab -u root -
+{ crontab -l -u root; echo '0 0 * * * ~/scripts/monitor.sh'; } | crontab -u root -
+{ crontab -l -u seronen; echo '0 4 * * SUN sudo ~/scripts/update.sh'; } | crontab -u seronen -
+{ crontab -l -u seronen; echo '@reboot sudo ~/scripts/update.sh'; } | crontab -u seronen -
+{ crontab -l -u seronen; echo '0 0 * * * ~/scripts/monitor.sh'; } | crontab -u seronen -
+{ crontab -e; echo '0 4 * * SUN sudo ~/scripts/update.sh'; } | crontab -e -
+{ crontab -e; echo '@reboot sudo ~/scripts/update.sh'; } | crontab -e -
+{ crontab -e; echo '0 0 * * * ~/scripts/monitor.sh'; } | crontab -e -
+
+#Apache2 setup
+sudo systemctl enable apache2
+rm -rf /var/www/html/
+cp -r ~/roger/assets/apache/ /var/www/html/
+
+#SSL Setup
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FI/ST=HEL/O=Hive/OU=roger-skyline-1/CN=192.168.1.195" -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
+cp ~/roger/assets/ssl/ssl-params.conf /etc/apache2/conf-available/ssl-params.conf
+sudo cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf.old
+rm /etc/apache2/sites-available/default-ssl.conf
+cp ~/roger/assets/ssl/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+rm /etc/apache2/sites-available/000-default.conf
+cp ~/roger/assets/ssl/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo a2enmod headers
+sudo a2ensite default-ssl
+sudo a2enconf ssl-params
+systemctl reload apache2
