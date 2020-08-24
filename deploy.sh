fc04e1b5f99adc24142ea0f54ac282297ea837ab
Samuli <samuli841@gmail.com>
2020-08-24 21:47:01 +0300

    basic html page added

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update -y && apt-get upgrade -y
-apt-get install sudo openssh-server ufw portsentry fail2ban apache2 mailutils -y
+apt-get install cron sudo openssh-server ufw portsentry fail2ban apache2 mailutils -y
 usermod -aG sudo seronen
 
 #Configuring Static IP Addr
@@ -12,8 +12,7 @@ cp ~/roger/assets/static_ip/interfaces /etc/network/interfaces
 rm -rf /etc/ssh/sshd_config
 cp ~/roger/assets/ssh/sshd_config /etc/ssh/
 sudo mkdir /home/seronen/.ssh/
-sudo mkdir /home/seronen/.ssh/authorized_keys
-sudo cat ~/roger/assets/ssh/id_rsa.pub > /home/seronen/.ssh/authorized_keys
+sudo cat ~/roger/assets/ssh/authorized_keys > /home/seronen/.ssh/
 
 sudo service ssh restart
 sudo service sshd restart
@@ -22,7 +21,7 @@ sudo ifup eth0
 
 #Fail2Ban setup
 cp ~/roger/assets/fail2ban/jail.local /etc/fail2ban/
-cp ~/roger/assets/fail2ban/http-sget-dos.conf /etc/fail2ban/filter.d
+cp ~/roger/assets/fail2ban/http-get-dos.conf /etc/fail2ban/filter.d
 cp ~/roger/assets/fail2ban/portscan.conf /etc/fail2ban/filter.d
 sudo service fail2ban restart
 
