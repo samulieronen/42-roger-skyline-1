cf15df3fad2fe76b2c43c191e75fec3b8d87adf7
Samuli <samuli841@gmail.com>
2020-08-24 15:12:15 +0300

    tredje

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+apt-get update -y && apt-get upgrade -y
+apt-get install sudo openssh-server ufw portsentry fail2ban apache2 mailutils -y
+usermod -aG sudo seronen
+
+#Configuring Static IP Addr
+
+cp ~/roger/assets/static_ip/interfaces /etc/network/interfaces
+
 #Configuing SSH
 rm -rf /etc/ssh/sshd_config
 cp roger/assets/sshd/sshd_config /etc/ssh/
 sudo mkdir /home/seronen/.ssh/
 sudo mkdir /home/seronen/.ssh/authorized_keys
-sudo mv ~/roger/assets/ssh/id_rsa.pub > /home/seronen/.ssh/authorized_keys
+sudo cat ~/roger/assets/ssh/id_rsa.pub > /home/seronen/.ssh/authorized_keys
 
 sudo service ssh restart
 sudo service sshd restart
@@ -13,7 +21,7 @@ sudo service networking restart
 sudo ifup eth0
 
 #Fail2Ban setup
-cp ~/roger/assets/fail2ban/jail.local /etc/fail2ban/jail.local
+cp ~/roger/assets/fail2ban/jail.local /etc/fail2ban/
 cp ~/roger/assets/fail2ban/http-sget-dos.conf /etc/fail2ban/filter.d
 cp ~/roger/assets/fail2ban/portscan.conf /etc/fail2ban/filter.d
 sudo service fail2ban restart
