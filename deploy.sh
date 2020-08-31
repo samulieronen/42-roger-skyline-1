7b19456fcbd70d7e04cbc50eb4504acb7e78a25a
Samuli <samuli841@gmail.com>
2020-08-31 20:59:13 +0300

    Mods to html and overall rework of deploy.sh and assets

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cp ~/roger/assets/static_ip/interfaces /etc/network/interfaces
 rm -rf /etc/ssh/sshd_config
 cp ~/roger/assets/ssh/sshd_config /etc/ssh/
 sudo mkdir /home/seronen/.ssh/
-cp ~/roger/assets/ssh/authorized_keys > /home/seronen/.ssh/
+cp ~/roger/assets/ssh/authorized_keys /home/seronen/.ssh/
 
 sudo service ssh restart
 sudo service networking restart
@@ -48,15 +48,17 @@ sudo systemctl disable syslog.service
 
 #Setup scripts
 cp -r ~/roger/assets/scripts/ ~/
-{ crontab -l -u root; echo '0 4 * * SUN sudo ~/scripts/update.sh'; } | crontab -u root -
-{ crontab -l -u root; echo '@reboot sudo ~/scripts/update.sh'; } | crontab -u root -
-{ crontab -l -u root; echo '0 0 * * * ~/scripts/monitor.sh'; } | crontab -u root -
-{ crontab -l -u seronen; echo '0 4 * * SUN sudo ~/scripts/update.sh'; } | crontab -u seronen -
-{ crontab -l -u seronen; echo '@reboot sudo ~/scripts/update.sh'; } | crontab -u seronen -
-{ crontab -l -u seronen; echo '0 0 * * * ~/scripts/monitor.sh'; } | crontab -u seronen -
-{ crontab -e; echo '0 4 * * SUN sudo ~/scripts/update.sh'; } | crontab -e -
-{ crontab -e; echo '@reboot sudo ~/scripts/update.sh'; } | crontab -e -
-{ crontab -e; echo '0 0 * * * ~/scripts/monitor.sh'; } | crontab -e -
+cp -r ~/roger/assets/scripts/ /home/seronen/
+chmod 755 ~/scripts/update.sh
+chmod 755 ~/scripts/cronmonitor.sh
+chmod 755 /home/seronen/scripts/update.sh
+chmod 755 /home/seronen/scripts/cronmonitor.sh
+{ crontab -l -u root; echo '0 4 * * SUN /root/scripts/update.sh'; } | crontab -u root -
+{ crontab -l -u root; echo '@reboot sleep 60 && /root/scripts/update.sh'; } | crontab -u root -
+{ crontab -l -u root; echo '0 0 * * * /root/scripts/monitor.sh'; } | crontab -u root -
+{ crontab -l -u seronen; echo '0 4 * * SUN /home/seronen/scripts/update.sh'; } | crontab -u seronen -
+{ crontab -l -u seronen; echo '@reboot sleep 60 && /home/seronen/scripts/update.sh'; } | crontab -u seronen -
+{ crontab -l -u seronen; echo '0 0 * * * /home/seronen/scripts/monitor.sh'; } | crontab -u seronen -
 
 #Apache2 setup
 sudo systemctl enable apache2
@@ -64,14 +66,17 @@ rm -rf /var/www/html/
 cp -r ~/roger/assets/apache2/ /var/www/html/
 
 #SSL Setup
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FI/ST=HEL/O=Hive/OU=roger-skyline-1/CN=192.168.1.195" -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FI/ST=HEL/O=Hive/OU=roger-skyline-1/CN=192.168.1.2" -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
 cp ~/roger/assets/ssl/ssl-params.conf /etc/apache2/conf-available/ssl-params.conf
 sudo cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf.old
 rm /etc/apache2/sites-available/default-ssl.conf
 cp ~/roger/assets/ssl/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 rm /etc/apache2/sites-available/000-default.conf
 cp ~/roger/assets/ssl/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo a2enmod ssl
 sudo a2enmod headers
 sudo a2ensite default-ssl
 sudo a2enconf ssl-params
 systemctl reload apache2
+
+#Now we should see a beautiful (lol) website! :)
